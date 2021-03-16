55d1f8570286d4694822f77c518ddb5629b24f98
xx01cyx <caoyuanxin0531@outlook.com>
2021-03-16 11:06:47 +0800

    feat: video view

--- UIPrototype/Karaoke/app/src/main/java/com/sjtu/karaoke/MainActivity.java
+++ UIPrototype/Karaoke/app/src/main/java/com/sjtu/karaoke/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -38,6 +39,9 @@ public class MainActivity extends AppCompatActivity {
         });
 
         chipNavigationBar.setItemSelected(R.id.viewSongs, true);
+
+        Intent intent = new Intent(this, AccompanySingActivity.class);
+        startActivity(intent);
     }
 
     @Override
--- UIPrototype/Karaoke/app/src/main/java/com/sjtu/karaoke/adapter/SongListAdapter.java
+++ UIPrototype/Karaoke/app/src/main/java/com/sjtu/karaoke/adapter/SongListAdapter.java
@@ -1,15 +1,19 @@
 package com.sjtu.karaoke.adapter;
 
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.ColorSpace;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.sjtu.karaoke.AccompanySingActivity;
 import com.sjtu.karaoke.Data;
 import com.sjtu.karaoke.R;
 
@@ -18,17 +22,17 @@ import java.util.List;
 
 public class SongListAdapter extends RecyclerView.Adapter<SongListAdapter.ViewHolder> {
     List<Data.Song> songs;
-
+    Context context;
     public class ViewHolder extends RecyclerView.ViewHolder {
         TextView songName, singer;
         ImageView image;
-
+        Button btnSing;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             songName = (TextView) itemView.findViewById(R.id.songName);
             singer = (TextView) itemView.findViewById(R.id.singer);
             image = (ImageView) itemView.findViewById(R.id.cover);
-
+            btnSing = (Button) itemView.findViewById(R.id.btnSing);
             // get the button also ?
         }
     }
@@ -40,6 +44,7 @@ public class SongListAdapter extends RecyclerView.Adapter<SongListAdapter.ViewHo
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View view = inflater.inflate(R.layout.song_row, parent, false);
         return new ViewHolder(view);
@@ -50,7 +55,13 @@ public class SongListAdapter extends RecyclerView.Adapter<SongListAdapter.ViewHo
         holder.songName.setText(songs.get(position).songName);
         holder.singer.setText(songs.get(position).singer);
         holder.image.setImageResource(songs.get(position).image);
-
+        holder.btnSing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, AccompanySingActivity.class);
+                context.startActivity(intent);
+            }
+        });
         // set button onClick listener ?
     }
 
