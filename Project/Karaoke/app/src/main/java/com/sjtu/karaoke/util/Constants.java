85c04a1f84928d89b094676e2bc3bcd28a477094
jyz-1201 <663283835@qq.com>
2021-06-02 11:43:30 +0800

    fix: 修复了钢琴评价系统不能正确读取和弦文件的bug

--- Project/Karaoke/app/src/main/java/com/sjtu/karaoke/util/Constants.java
+++ Project/Karaoke/app/src/main/java/com/sjtu/karaoke/util/Constants.java
@@ -73,7 +73,7 @@ public class Constants {
     public static final String AUTHORITY = PACKAGE_NAME + ".fileprovider";
 
     // request URL
-    public static final String SERVER_IP = "http://192.168.10.9:5000";
+    public static final String SERVER_IP = "http://192.168.1.125:5000";
 
     public static final String GET_FILE_ROOT_URL = SERVER_IP + "/getFile";
 
