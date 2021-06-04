20f20a68f96608a7b7a7935281ce6866cd333b01
jyz-1201 <663283835@qq.com>
2021-06-04 17:10:17 +0800

    tweak: 调整歌唱打分系统的评分参数

--- Project/Karaoke/SingRater/src/main/cpp/setting.h
+++ Project/Karaoke/SingRater/src/main/cpp/setting.h
@@ -31,11 +31,11 @@ const float OnceRightScore = 1.25;
 const float OnceNotCorrectScore = 0.3;
 const float OnceWrongScore = 0.9;
 const float f0Shift = 0.1;
-const float STANDARD_EMOTION_RATE = 1.8;
+const float STANDARD_EMOTION_RATE = 2.35;//standard越高，得分越低
 const float EMOTION_SCORE_SCALE = 3.5;
-const float STANDARD_END_BLANK_DELTA = 3;
-const float END_BLANK_SCORE_SCALE = 2;
-const float STANDARD_CONTINUOUS_BLANK_DELTA = 2;
+const float STANDARD_END_BLANK_DELTA = 2.8;//standard越高，得分越高
+const float END_BLANK_SCORE_SCALE = 1.5;
+const float STANDARD_CONTINUOUS_BLANK_DELTA = 2.15;//standard越高，得分越高
 const float CONTINUOUS_BLANK_SCORE_SCALE = 2.5;
 const float MULTI_SCORE_START_LOWERBOUND = 50;
 
