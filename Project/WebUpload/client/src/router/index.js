0ff6274eb410e171eaeb9fc578cbed714e1b30f2
xx01cyx <caoyuanxin0531@outlook.com>
2021-06-25 11:46:33 +0800

    fix: disable entering login page if authed

--- Project/WebUpload/client/src/router/index.js
+++ Project/WebUpload/client/src/router/index.js
@@ -35,6 +35,14 @@ const router = new Router({
 });
 
 router.beforeEach((to, from, next) => {
+  if (to.path == '/login') {
+    const user = localStorage.getItem('user');
+    if (user && Object.keys(user).length !== 0) {
+      next({
+        path: '/songs',
+      });
+    }
+  }
   if (to.matched.some((r) => r.meta.requireAuth)) {
     const user = localStorage.getItem('user');
     if (user && Object.keys(user).length !== 0) {
